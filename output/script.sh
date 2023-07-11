38b50d94f0754451a179250f54446f6a96504145
icetee4eva <30740369+prablordeppey@users.noreply.github.com>
2023-07-11 08:03:16 +0100

    initial comit for a umbrella chart mulip aliases

---
+++ output/script.sh
@@ -0,0 +1,13 @@
+helm dependency update
+helm dependency build
+helm dependency list
+
+helm template . > output/rendered.yaml
+
+helm repo search
+helm repo list
+
+helm search repo > output/stable-charts.csv
+
+helm dependency update 
+
